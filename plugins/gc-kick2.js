a9bdd09a5c0a3a8b481ad7c2c64498bcdc107be7
EmeEsteban <123786143+EmeEsteban@users.noreply.github.com>
2024-01-04 15:25:06 -0300

    Update gc-kick2.js

--- plugins/gc-kick2.js
+++ plugins/gc-kick2.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, participants, usedPrefix, command}) => {
   if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] 𝙴𝙻 𝙾𝚆𝙽𝙴𝚁 𝚃𝙸𝙴𝙽𝙴 𝚁𝙴𝚂𝚃𝚁𝙸𝙽𝙶𝙸𝙳𝙾 (𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝 / 𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙴𝙻 𝚄𝚂𝙾 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*';
     const kicktext = ``;
-  if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, {mentions: conn.parseMention(kicktext)});
+  if (!m.mentionedJid[0] && !m.quoted) return m.reply(, m.chat, {mentions: conn.parseMention()});
   if (m.mentionedJid.includes(conn.user.jid)) return;
   const user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
   const owr = m.chat.split`-`[0];
